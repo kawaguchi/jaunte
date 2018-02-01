61a70bd6d5592dfe70920d2f72a4089c2560a1e2
kawaguchi <masayakawaguchi@gmail.com>
2018-02-02 04:18:59 +0900

    Fix header

--- jaunte.el
+++ jaunte.el
@@ -1,16 +1,36 @@
-;;; jaunte.el --- Emacs Hit a Hint
+;;; jaunte.el --- cursor teleportation
 
+;; Copyright (C) 2010-2018 by Masaya Kawaguchi
+
+;; Author   : Masaya Kawaguchi <masayakawaguchi@gmail.com>
+;; URL      : https://github.com/kawaguchi/jaunte.el
+;; Version  : 1.0.0
+;; Keywords : cursor
 ;; Package-Requires: ((cl-lib "0.3"))
 
-;; 参考
-;; yafastnav.el
-;; vimperator
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
 
-;; 設定
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Usage
+;;
 ;; (require 'jaunte)
 ;; (global-set-key (kbd "C-c C-j") 'jaunte)
 
+;;; Code:
 (require 'cl-lib)
+
 (defvar jaunte-keys (mapcar #'identity "jklasdfghyuiopqwertnmzxcvb"))
 
 (defface jaunte-hint-face
